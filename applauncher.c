4df22e8bb7e6b2c21d65ca0599fcb36094b731fd
Matt <cheatfreak47@gmail.com>
2016-01-30 10:45:24 -0500

    Initial Commit

---
+++ applauncher.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int
+main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		fprintf(stderr, "Command ran without arguments...\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (!strcmp(argv[1], "-bootgame"))
+		execl("sonic.exe", "sonic.exe", NULL);
+	else if (!strcmp(argv[1], "-bootlauncher"))
+		execl("SADXModManager.exe", "SADXModManager.exe", NULL);
+	else
+	{
+		fprintf(stderr, "Unknown option: \"%s\"\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	return EXIT_SUCCESS;
+}
